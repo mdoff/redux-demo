05f071c90c565e8b8fa7ba5cce5aca49a8b0e526
Tomasz Lewinski <tomasz.lewinski@scalac.io>
2016-05-05 08:57:16 +0200

    Readme

---
+++ README.md
@@ -0,0 +1,14 @@
+#Redux demo app
+
+## Installation
+```
+git clone https://github.com/mdoff/redux-demo.git
+cd redux-demo
+npm install
+npm start
+```
+Visit [http://localhost:8080](http://localhost:8080)
+
+
+## About
+This is a react - redux demo app that allows you to create and edit notes.
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './components/app.js';
-import React from 'react';
 import { createStore, combineReducers } from 'redux';
 import 'bootstrap-css-only/css/bootstrap.css';
 
+import App from './components/app.js';
+import React from 'react';
 import reducers from './reducers';
-import {addNote, removeNote, editNote} from './actions';
 
 const store = createStore(reducers, window.devToolsExtension ? window.devToolsExtension() : f => f);
 
